148d9829443cbb92850c0b769fb211dd99903d64
MCSipahioglu <81450182+MCSipahioglu@users.noreply.github.com>
2022-10-10 03:47:42 +0300

    Implemented Responsive Website (By making the working area square)
    Also got the letter "O" dead center

--- js/swatch.js
+++ js/swatch.js
@@ -1,20 +1,22 @@
-const swatch_element=document.getElementById("landing_swatch_container")
-const swatch_element_count=4
+const swatch_element=document.getElementById("landing_swatch_container");
+const angle_step=25;
+const swatch_element_count=4;
 var current_step=0;
 
-document.addEventListener("wheel", swatch_rotate)
+
+document.addEventListener("wheel", swatch_rotate);
 
 function swatch_rotate(event){
     var delta = Math.sign(event.deltaY);     //Normalize Scroll's deltaY (+-120 for Chrome into -+1)
 
     if (delta==1 && current_step<swatch_element_count-1){         //Rotate Down
-        swatch_element.style.transform = "rotate("+ (current_step*-15-15) +"deg)";
+        swatch_element.style.transform = "rotate("+ (current_step*(-angle_step)-angle_step) +"deg)";
         current_step+=1;
         console.log('Down');
         console.log(current_step);
     }
     if (delta==-1 && current_step>0){                           //Rotate Up
-        swatch_element.style.transform = "rotate("+ (current_step*-15+15) +"deg)";
+        swatch_element.style.transform = "rotate("+ (current_step*(-angle_step)+angle_step) +"deg)";
         current_step+=-1;
         console.log('Up');
         console.log(current_step);
