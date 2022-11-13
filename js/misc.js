8084f7fe668fa1ff58fdd466c8e55579deca42cb
MCSipahioglu <81450182+MCSipahioglu@users.noreply.github.com>
2022-11-14 00:55:46 +0200

    Added loading screen

--- js/misc.js
+++ js/misc.js
@@ -1,7 +1,13 @@
-document.getElementById("logo_email").onclick = function() {copyEmail()};
+document.getElementById("logo_email").onclick = function() {CopyEmail()};
+
+window.onload = function() {LoadingScreen()};
+
+function LoadingScreen(){
+    document.getElementById("loader_wrapper").style.opacity="0%";
+}
 
 
-function copyEmail() {
+function CopyEmail() {
     /* Copy text into clipboard */
     navigator.clipboard.writeText("mustafacagataysipahioglu@gmail.com");
     alert("Copied contact e-mail.");
