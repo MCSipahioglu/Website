de06f29bb941560f9e3d492cef1d32c47077d581
MCSipahioglu <81450182+MCSipahioglu@users.noreply.github.com>
2022-11-13 02:49:10 +0200

    Responsive done until square screen. Doing logos

---
+++ js/page_changer.js
@@ -0,0 +1,24 @@
+const element_roboticist=document.getElementById("swatch_roboticist");
+const element_botsmith=document.getElementById("swatch_botsmith");
+const element_inventor=document.getElementById("swatch_inventor");
+const element_writer=document.getElementById("swatch_writer");
+const element_traveller=document.getElementById("swatch_traveller");
+
+const element_landing_header_name=document.getElementById("landing_header_name");
+const element_landing_header_surname=document.getElementById("landing_header_surname");
+
+
+element_roboticist.addEventListener("click", page_change_roboticist);
+
+
+
+function page_change_roboticist(event){
+    console.log('Roboticist Clicked!');
+
+
+}
+
+
+
+
+
--- js/swatch.js
+++ js/swatch.js
@@ -1,6 +1,6 @@
-const swatch_element=document.getElementById("landing_swatch_container");
+const element_swatch=document.getElementById("landing_swatch_container");
 const angle_step=25;
-const swatch_element_count=5;
+const element_swatch_count=5;
 var current_step=0;
 
 
@@ -9,14 +9,14 @@ document.addEventListener("wheel", swatch_rotate);
 function swatch_rotate(event){
     var delta = Math.sign(event.deltaY);     //Normalize Scroll's deltaY (+-120 for Chrome into -+1)
 
-    if (delta==1 && current_step<swatch_element_count-1){         //Rotate Down
-        swatch_element.style.transform = "rotate("+ (current_step*(-angle_step)-angle_step) +"deg)";
+    if (delta==1 && current_step<element_swatch_count-1){         //Rotate Down
+        element_swatch.style.transform = "rotate("+ (current_step*(-angle_step)-angle_step) +"deg)";
         current_step+=1;
         console.log('Down');
         console.log(current_step);
     }
     if (delta==-1 && current_step>0){                           //Rotate Up
-        swatch_element.style.transform = "rotate("+ (current_step*(-angle_step)+angle_step) +"deg)";
+        element_swatch.style.transform = "rotate("+ (current_step*(-angle_step)+angle_step) +"deg)";
         current_step+=-1;
         console.log('Up');
         console.log(current_step);
