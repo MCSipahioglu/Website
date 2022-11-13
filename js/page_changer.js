89b10ef615caf4ac1d4d5839ebf6f95a2e14aa43
MCSipahioglu <81450182+MCSipahioglu@users.noreply.github.com>
2022-11-13 18:48:31 +0200

    Sweeper Done, working on logo sweeper

--- js/page_changer.js
+++ js/page_changer.js
@@ -7,13 +7,13 @@ const element_traveller=document.getElementById("swatch_traveller");
 const element_landing_header_name=document.getElementById("landing_header_name");
 const element_landing_header_surname=document.getElementById("landing_header_surname");
 
+const element_sweeper_wrapper=document.getElementById("sweeper_wrapper");
+
 
 element_roboticist.addEventListener("click", page_change_roboticist);
-
-
-
 function page_change_roboticist(event){
     console.log('Roboticist Clicked!');
+    element_sweeper_wrapper.style.left="-0.75vmin";
 
 
 }
