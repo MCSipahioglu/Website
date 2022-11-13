9b1882fcfe4c64e2093e110be678a24057afa4a9
MCSipahioglu <81450182+MCSipahioglu@users.noreply.github.com>
2022-11-14 00:59:05 +0200

    Finished the placeholder loading screen

--- js/misc.js
+++ js/misc.js
@@ -4,6 +4,7 @@ window.onload = function() {LoadingScreen()};
 
 function LoadingScreen(){
     document.getElementById("loader_wrapper").style.opacity="0%";
+    document.getElementById("loader_wrapper").style.zIndex="0";
 }
 
 
