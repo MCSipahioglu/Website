731a72a6cb7275bcfa609505e4ef7dbf6ecd056a
MCSipahioglu <81450182+MCSipahioglu@users.noreply.github.com>
2022-11-14 23:58:25 +0200

    Removed swatch's extra step size

--- js/swatch.js
+++ js/swatch.js
@@ -1,5 +1,4 @@
 const element_swatch=document.getElementById("landing_swatch_wrapper");
-const angle_step=25;
 const element_swatch_count=5;
 var current_step=0;
 
@@ -10,13 +9,13 @@ function swatch_rotate(event){
     var delta = Math.sign(event.deltaY);     //Normalize Scroll's deltaY (+-120 for Chrome into -+1)
 
     if (delta==1 && current_step<element_swatch_count-1){         //Rotate Down
-        element_swatch.style.transform = "rotate("+ (current_step*(-angle_step)-angle_step) +"deg)";
+        element_swatch.style.transform = "rotate(calc(" + -1*current_step + " * var(--swatch_angle_step) - var(--swatch_angle_step)))";
         current_step+=1;
         console.log('Down');
         console.log(current_step);
     }
     if (delta==-1 && current_step>0){                           //Rotate Up
-        element_swatch.style.transform = "rotate("+ (current_step*(-angle_step)+angle_step) +"deg)";
+        element_swatch.style.transform = "rotate(calc(" + -1*current_step + " * var(--swatch_angle_step) + var(--swatch_angle_step)))";
         current_step+=-1;
         console.log('Up');
         console.log(current_step);
