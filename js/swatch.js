611fa0c4bbb5d821488d9aaea811727097f8664b
MCSipahioglu <81450182+MCSipahioglu@users.noreply.github.com>
2022-11-19 21:48:22 +0200

    Landing freedom reverted

--- js/swatch.js
+++ js/swatch.js
@@ -17,39 +17,6 @@ function RotateSwatch(event){
         element_swatch.style.transform = "rotate(calc(" + -1*current_step + " * var(--swatch_angle_step) + var(--swatch_angle_step)))";
         current_step+=-1;
     }
-
-    if(current_step==0){
-        e_roboticist.style.pointerEvents="all";
-        e_botsmith.style.pointerEvents="none";
-        e_inventor.style.pointerEvents="none";
-        e_writer.style.pointerEvents="none";
-        e_traveller.style.pointerEvents="none";
-    }else if(current_step==1){
-        e_roboticist.style.pointerEvents="none";
-        e_botsmith.style.pointerEvents="all";
-        e_inventor.style.pointerEvents="none";
-        e_writer.style.pointerEvents="none";
-        e_traveller.style.pointerEvents="none";
-    }else if(current_step==2){
-        e_roboticist.style.pointerEvents="none";
-        e_botsmith.style.pointerEvents="none";
-        e_inventor.style.pointerEvents="all";
-        e_writer.style.pointerEvents="none";
-        e_traveller.style.pointerEvents="none";
-    }else if(current_step==3){
-        e_roboticist.style.pointerEvents="none";
-        e_botsmith.style.pointerEvents="none";
-        e_inventor.style.pointerEvents="none";
-        e_writer.style.pointerEvents="all";
-        e_traveller.style.pointerEvents="none"; 
-    }else if(current_step==4){
-        e_roboticist.style.pointerEvents="none";
-        e_botsmith.style.pointerEvents="none";
-        e_inventor.style.pointerEvents="none";
-        e_writer.style.pointerEvents="none";
-        e_traveller.style.pointerEvents="all"; 
-    }
-
 }
 
 //I really thought I wouldn't be able to build the swatch but that wasn't so bad once I got the idea that I could rotate the general div.
