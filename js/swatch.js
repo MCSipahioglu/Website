5e9087f5b40822f38bb482be1751cf591511086a
MCSipahioglu <81450182+MCSipahioglu@users.noreply.github.com>
2022-11-19 15:13:22 +0200

    (Maybe Revert Later)) Landing Swatcch now can only click the inline one.

--- js/swatch.js
+++ js/swatch.js
@@ -1,3 +1,4 @@
+//Swatch element to rotate
 const element_swatch=document.getElementById("landing_swatch_wrapper");
 const element_swatch_count=5;
 var current_step=0;
@@ -15,6 +16,38 @@ function RotateSwatch(event){
     else if (delta==-1 && current_step>0){                           //Rotate Up
         element_swatch.style.transform = "rotate(calc(" + -1*current_step + " * var(--swatch_angle_step) + var(--swatch_angle_step)))";
         current_step+=-1;
+    }
+
+    if(current_step==0){
+        e_roboticist.style.pointerEvents="all";
+        e_botsmith.style.pointerEvents="none";
+        e_inventor.style.pointerEvents="none";
+        e_writer.style.pointerEvents="none";
+        e_traveller.style.pointerEvents="none";
+    }else if(current_step==1){
+        e_roboticist.style.pointerEvents="none";
+        e_botsmith.style.pointerEvents="all";
+        e_inventor.style.pointerEvents="none";
+        e_writer.style.pointerEvents="none";
+        e_traveller.style.pointerEvents="none";
+    }else if(current_step==2){
+        e_roboticist.style.pointerEvents="none";
+        e_botsmith.style.pointerEvents="none";
+        e_inventor.style.pointerEvents="all";
+        e_writer.style.pointerEvents="none";
+        e_traveller.style.pointerEvents="none";
+    }else if(current_step==3){
+        e_roboticist.style.pointerEvents="none";
+        e_botsmith.style.pointerEvents="none";
+        e_inventor.style.pointerEvents="none";
+        e_writer.style.pointerEvents="all";
+        e_traveller.style.pointerEvents="none"; 
+    }else if(current_step==4){
+        e_roboticist.style.pointerEvents="none";
+        e_botsmith.style.pointerEvents="none";
+        e_inventor.style.pointerEvents="none";
+        e_writer.style.pointerEvents="none";
+        e_traveller.style.pointerEvents="all"; 
     }
 
 }
