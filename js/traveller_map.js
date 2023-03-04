ca8ac6dc49f70a1ba98f1e948b097b2f7dcd820c
MCSipahioglu <81450182+MCSipahioglu@users.noreply.github.com>
2023-03-04 02:20:46 +0100

    Map, Zoom, Country Tooltips

---
+++ js/traveller_map.js
@@ -0,0 +1,39 @@
+e_map=document.getElementById("world_map")
+
+
+
+document.addEventListener("wheel", MapZoom, true);
+
+
+
+
+
+
+
+
+
+
+
+
+zoom=4.703125;
+const zoom_speed=0.3125;
+
+
+
+function MapZoom(event){
+    prev_zoom=zoom;
+    if(event.deltaY>0 && (zoom-prev_zoom*zoom_speed<1.3125)){   //Don't try to zoom out if we are maxed zoomed out.
+        zoom=1.3125;
+    }else{
+        if(event.deltaY<0){
+            zoom+=prev_zoom*zoom_speed;
+        }else{
+            zoom-=prev_zoom*zoom_speed;
+        }     
+    }
+    e_map.style.transform="scale("+ zoom +")";
+
+    console.log(zoom);
+
+
+}
