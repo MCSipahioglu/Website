3fc9eb2b3e31c0ae24c6715fac8ed57238bb37c2
MCSipahioglu <81450182+MCSipahioglu@users.noreply.github.com>
2023-03-08 01:41:31 +0100

    Added twitter and insta links to mobile

--- js/mobile_sidebar.js
+++ js/mobile_sidebar.js
@@ -2,6 +2,8 @@ const em_logo_dots=document.getElementById("mobile_logo_dots");
 const em_logo_cv=document.getElementById("mobile_logo_cv");
 const em_logo_lang=document.getElementById("mobile_logo_lang");
 const em_logo_email=document.getElementById("mobile_logo_email");
+const em_logo_instagram=document.getElementById("mobile_logo_instagram");
+const em_logo_twitter=document.getElementById("mobile_logo_twitter");
 const em_logo_kofi=document.getElementById("mobile_logo_kofi");
 
 const em_notification_wrapper=document.getElementById("mobile_notification");
@@ -9,11 +11,7 @@ const em_notification_text=document.getElementById("mobile_notification_text");
 
 var sidebar_active=0;
 var lang_active=0;
-var sidebar_delay_step=30;
-
-
-
-
+var sidebar_delay_step=6;
 
 
 function MobileSidebarToggle(){
@@ -21,19 +19,23 @@ function MobileSidebarToggle(){
     if(sidebar_active==0){
         sidebar_active=1;
         em_logo_dots.style.transform="rotate(+180deg)";
-        setTimeout(() => {  em_logo_cv.style.transform="translateX(0vmin)"; },    0*sidebar_delay_step);
-        setTimeout(() => {  em_logo_lang.style.transform="translateX(0vmin)"; },  1*sidebar_delay_step);
-        setTimeout(() => {  em_logo_email.style.transform="translateX(0vmin)"; }, 2*sidebar_delay_step);
-        setTimeout(() => {  em_logo_kofi.style.transform="translateX(0vmin)"; },  3*sidebar_delay_step);
+        em_logo_cv.style.transform="translateX(0vmin)";
+        em_logo_lang.style.transform="translateX(0vmin)";
+        em_logo_email.style.transform="translateX(0vmin)";
+        em_logo_instagram.style.transform="translateX(0vmin)";
+        em_logo_twitter.style.transform="translateX(0vmin)";
+        em_logo_kofi.style.transform="translateX(0vmin)";
     }
 
     else if(sidebar_active==1){
         sidebar_active=0;
         em_logo_dots.style.transform="rotate(0deg)";
         em_logo_cv.style.transform="translateX(12vmin)";
-        em_logo_lang.style.transform="translateX(12vmin)";
-        em_logo_email.style.transform="translateX(12vmin)";
-        em_logo_kofi.style.transform="translateX(12vmin)";
+        em_logo_lang.style.transform="translateX("+(12+1*sidebar_delay_step)+"vmin)";
+        em_logo_email.style.transform="translateX("+(12+2*sidebar_delay_step)+"vmin)";
+        em_logo_instagram.style.transform="translateX("+(12+3*sidebar_delay_step)+"vmin)";
+        em_logo_twitter.style.transform="translateX("+(12+4*sidebar_delay_step)+"vmin)";
+        em_logo_kofi.style.transform="translateX("+(12+5*sidebar_delay_step)+"vmin)";
     }
     
 }
@@ -83,6 +85,8 @@ function MobileSidebarColor(color) {
     em_logo_cv.style.backgroundColor=color;
     em_logo_lang.style.backgroundColor=color;
     em_logo_email.style.backgroundColor=color;
+    em_logo_instagram.style.backgroundColor=color;
+    em_logo_twitter.style.backgroundColor=color;
     em_logo_kofi.style.backgroundColor=color;
 
 }
