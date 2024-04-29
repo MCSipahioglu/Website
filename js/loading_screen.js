06133e5eedd0eaf52d6eb731ed1908029738c8d1
MCSipahioglu <81450182+MCSipahioglu@users.noreply.github.com>
2024-04-30 00:18:43 +0200

    Loading Animation!

--- js/loading_screen.js
+++ js/loading_screen.js
@@ -1,5 +1,7 @@
 //For Loading the Screen
-window.onload = function() {LoadingScreen()};
+window.onload = function() {
+    setTimeout(LoadingScreen, 1000); // Delay execution by 1 seconds
+    };
 
 function LoadingScreen(){
     document.getElementById("loader_wrapper").style.opacity="0%";
